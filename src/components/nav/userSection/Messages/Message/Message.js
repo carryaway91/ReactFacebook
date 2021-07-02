6b68217243ce20a2b13b05b1aabea562cb3f3633
carryaway91 <carryaway91@gmail.com>
2021-07-02 18:26:04 +0200

    added online status

--- src/components/nav/userSection/Messages/Message/Message.js
+++ src/components/nav/userSection/Messages/Message/Message.js
@@ -10,7 +10,7 @@ const Message = props => {
     return (
         <InfoContainer>
             <div style={{ display: 'flex', width: '100%' }} onClick={() => showChat(props.friend)}>
-                <UserCircle w="60px" photo={props.friend.photo}/>
+                <UserCircle size="md" online w="60px" photo={props.friend.photo}/>
                 <div style={{ marginLeft: '.5rem', display: 'flex', flexDirection: 'column', justifyContent: 'center'}}>
                     <h4 style={{ margin: 0}}>{props.friend.fName + ' ' + props.friend.lName} </h4>
                     <p style={{ margin: 0}}>Say hi to {props.friend.fName + ' ' + props.friend.lName}!</p>
